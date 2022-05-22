bf22a06de68f13a6670c9d8ef968f5753e1861d7
Dylan Neve <dylanneve1@gmail.com>
2022-05-22 12:46:33 +0100

    starlte: Drop sound_trigger.primary.exynos9810 and patch audio.primary.exynos9810
    * Patch to remove the dependency on libaudio_soundtrigger

--- starlte-vendor.mk
+++ starlte-vendor.mk
@@ -56,7 +56,6 @@ PRODUCT_PACKAGES += \
     libopenvx \
     libaudioeffectoffload \
     audio.primary.exynos9810 \
-    sound_trigger.primary.exynos9810 \
     lib_SamsungRec_06006 \
     libaudioproxy \
     librecordalive \
